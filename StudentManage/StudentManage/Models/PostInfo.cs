185899c8d1ee84802b27ebfacaa38e10d72929b4
3166747617 <3166747617@qq.com>
2025-04-22 16:25:44 +0800

    Add files via upload

---
+++ StudentManage/StudentManage/Models/PostInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StudentManage.Models
+{
+    public partial class PostInfo
+    {
+        public int Id { get; set; }
+        public string PostName { get; set; }
+    }
+}
---
+++ StudentManage/StudentManage/Models/ProfessionalInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StudentManage.Models
+{
+    public partial class ProfessionalInfo
+    {
+        public int Id { get; set; }
+        public string ProName { get; set; }
+    }
+}
